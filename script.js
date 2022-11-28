65dc748d7413823b0992c083a0d66397edec1fa3
Collins Curran <currancollins22@gmail.com>
2022-11-28 12:58:52 -0800

    push to create live site

--- script.js
+++ script.js
@@ -5,42 +5,97 @@
 //pword matches selected criteria
 //pword displayed in an alert or written on page
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
+// var passwordLength = prompt("How long would you like your password to be? Must be 8-128 characters.");
+
+var passwordLen = 0
+
+var pword = ""
+var lcase = ["abcdefghijklmnopqrstuvwxyz"]
+var ucase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+var numbers = ["0123456789"]
+var specSymbols = ["!@#$%^&*()-_+=:;?/>.<,"]
+
 // Write password to the #password input
+
+function generatePassword() {
+
+  var passwordLength = prompt("How long would you like your password to be? Must be 8-128 characters.");
+  while (passwordLength <= 7 || passwordLength >= 129) {
+    alert("Password length does not meet requirements must be 8-128 characters.");
+    var passwordLength = prompt("How long would you like your password to be? Must be 8-128 characters.");
+  } if (isNaN(passwordLength)) {
+    alert("Must input a number from 8-128.");
+    var passwordLength = prompt("How long would you like your password to be? Must be 8-128 characters.");
+  }
+
+  var lowercase = confirm("Would you like to use Lower Case characters?");
+  console.log(lowercase);
+
+  var uppercase = confirm("Would you like to use Upper Case characters?");
+  console.log(uppercase);
+
+  var numeric = confirm("Would you like to use Numeric characters?")
+  console.log(numeric);
+
+  var specialSymbols = confirm("Would you like to use Special characters?");
+  console.log(specialSymbols);
+
+  while (lowercase == false && uppercase == false && numeric == false && specialSymbols == false) {
+    alert("Please select atleast one criteria.");
+    var lowercase = confirm("Would you like to use Lower Case characters?");
+    var uppercase = confirm("Would you like to use Upper Case characters?");
+    var numeric = confirm("Would you like to use Numeric characters?");
+    var specialSymbols = confirm("Would you like to use Special characters?");
+  }
+
+  var pwordcrit = []
+  var pwordindex = 0;
+
+
+  if (lowercase) {
+    pwordcrit = pwordcrit.concat(lcase)
+  }
+
+  if (uppercase) {
+    pwordcrit = pwordcrit.concat(ucase)
+  }
+
+  if (numeric) {
+    pwordcrit = pwordcrit.concat(numbers)
+  }
+
+  if (specialSymbols) {
+    pwordcrit = pwordcrit.concat(specSymbols)
+  }
+
+
+  for (let i = pwordindex; i < passwordLength; i++) {
+    pwordcrit.push(
+      pwordcrit[Math.floor(Math.random() * pwordindex)]
+    );
+
+  }
+
+  var password = "";
+
+  for (let i = 0; i < pwordcrit.length; i++) {
+    var passwordLength = pwordcrit[i];
+    password +=
+      passwordLength[Math.floor(Math.random() * passwordLength.length)
+      ];
+  }
+
+  return password;
+
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  //var length = (>=8, <=128);
-  //var lcase = ["abcdefghijklmnopqrstuvwxyz"];
-  //var ucase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-  //var numbers = ["1234567890"];
-  //var specSymbols = ["`~!@#$%^&*()_-+=|\}{][':;/?.>,<"]
-  //var pword = "";
-  //var character = "";
 
-  //  while (pword.length <= length) {
-  //   entity1 = Math.ceil(lcase.length * Math.random() * Math.random());
-  //   entity2 = Math.ceil(ucase.length * Math.random() * Math.random());
-  //   entity3 = Math.ceil(numbers.length * Math.random() * Math.random());
-  //   entity4 = Math.ceil(specSymbols.length * Math.random() * Math.random());
-  //   hold = lcase.charAt(entity1);
-  //   hold = pword.length % 8 == 128 ? hold.toUpperCase() : hold;
-  //   character += hold;
-  //   character += ucase.charAt(entity2);
-  //   character += numbers.charAt(entity3);
-  //   character += specSymbols.charAt(entity4);
-  //   pword = character;}
-
-  // pword = pword
-  // .split("")
-  // .sort(function() {
-  //   return 0.5 - Math.random();
-  // })
-  // .join("");
-  // return pword.substr(0, len);
-
- // }
 
   passwordText.value = password;
 
@@ -49,55 +104,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var passwordLength = prompt("How long would you like your password to be? Must be 8-128.");
-console.log(passwordLength);
-
-var lowercase = confirm("Would you like to use Lower Case characters?");
-console.log(lowercase);
-
-var uppercase = confirm("Would you like to use Upper Case characters?");
-console.log(uppercase);
-
-var numeric = confirm("Would you like to use Numeric characters?")
-console.log(numeric);
-
-var specialSymbols = confirm("Would you like to use Special characters?");
-console.log(specialSymbols);
-
-
-var lcase = ["abcdefghijklmnopqrstuvwxyz"]
-console.log(lcase);
-
-var ucase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-console.log(ucase);
-
-var numbers = ["1234567890"]
-console.log(numbers);
-
-
-var specSymbols = ["`~!@#$%^&*()_-+=|\}{][':;/?.>,<"]
-//console.log(specsymbols);
-
-//var length = (>=8, <=128);
 
 
 
-if (lowercase == true) {
-  //include var lcase
-  passwordText += lcase;
-}
-
-if (uppercase == true) {
-  //include var ucase
-  passwordText += ucase;
-}
-
-if (numeric == true) {
-  //include var numbers
-  passwordText += numbers;
-}
-
-if (specialSymbols == true) {
-  //include var specSymbols
-  passwordText += specSymbols;
-}
